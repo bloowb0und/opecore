2982ecfa9d888c0583d2fc3d26f3658c04ad1ce9
Kirill <kirill@futurix.solutions>
2024-07-26 02:37:41 +0200

    ⚡ Added Suspense and a loader.

--- src/app/(main-page)/components/sections/resources-section.tsx
+++ src/app/(main-page)/components/sections/resources-section.tsx
@@ -1,15 +1,19 @@
 ﻿import { ResourcesContainer } from '@/app/(main-page)/components/resources-container';
 import { SearchInputForm } from '@/app/(main-page)/components/search-input-form';
 import { TabSelector } from '@/app/(main-page)/components/tab-selector';
+import { Suspense } from 'react';
+import { Loader } from '@/app/components/loader';
 
 export const ResourcesSection = () => {
   return (
     <section className="flex w-full flex-col items-center gap-16">
-      <div className="flex w-full flex-col gap-8">
-        <SearchInputForm />
-        <TabSelector />
-      </div>
-      <ResourcesContainer />
+      <Suspense fallback={<Loader />}>
+        <div className="flex w-full flex-col gap-8">
+          <SearchInputForm />
+          <TabSelector />
+        </div>
+        <ResourcesContainer />
+      </Suspense>
     </section>
   );
 };
---
+++ src/app/components/loader.tsx
@@ -0,0 +1,1 @@
+﻿export const Loader = () => <div className="loader" />;
